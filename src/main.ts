1e0b86f0831fbd76fdb42d981908be090e5c1ad4
sebasGZA <sebassga34@gmail.com>
2026-09-01 15:48:17 -0500

    feat: install swagger and validations

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { NestFactory } from '@nestjs/core';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 import { AppModule } from './app.module';
+import { swaggerConfiguration } from './swagger/config/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 3000;
+  const prefix = process.env.API_PREFIX ?? 'api';
+
+  swaggerConfiguration(app);
+  app.setGlobalPrefix(prefix);
+    app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
+
   await app.listen(port, () => {
     Logger.log(`Server is running on port ${port}`, 'main')
   });
