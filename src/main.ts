da6b62ebb03b1b34f065dbfb7d6a78d0ff9ad785
sebasGZA <sebassga34@gmail.com>
2026-09-01 15:20:22 -0500

    feat: create docker compose to mount a postgres db container

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, () => {
+    Logger.log(`Server is running on port ${port}`, 'main')
+  });
 }
 bootstrap();
