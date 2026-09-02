24c52f40391eccab443ac7919617104079fcaee1
sebasGZA <sebassga34@gmail.com>
2026-09-02 16:53:06 -0500

    feat: implement validation by roles

--- src/priority/infrastructure/http/priority.controller.ts
+++ src/priority/infrastructure/http/priority.controller.ts
@@ -1,13 +1,17 @@
 import { Controller, Get } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
 
 import { PriorityService } from "../../application/services/priority.service";
+import { Roles } from "../../../auth/infrastructure/decorators/roles.decorator";
+import { RoleEnum } from "../../../role/domain/enums/role.enum";
 
+@ApiBearerAuth()
 @ApiTags('Priorities')
 @Controller('priorities')
 export class PriorityController {
     constructor(private readonly priorityService: PriorityService) { }
 
+    @Roles(RoleEnum.ADMIN, RoleEnum.SOPORTE, RoleEnum.SUPERVISOR)
     @Get()
     getPriorities() {
         return this.priorityService.getAll()
--- src/status/infrastructure/http/status.controller.ts
+++ src/status/infrastructure/http/status.controller.ts
@@ -1,13 +1,17 @@
 import { Controller, Get } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
 
 import { StatusService } from "../../application/services/status.service";
+import { RoleEnum } from "../../../role/domain/enums/role.enum";
+import { Roles } from "../../../auth/infrastructure/decorators/roles.decorator";
 
+@ApiBearerAuth()
 @ApiTags('Statuses')
 @Controller('statuses')
 export class StatusController {
     constructor(private readonly statusService: StatusService) { }
 
+    @Roles(RoleEnum.ADMIN, RoleEnum.SOPORTE, RoleEnum.SUPERVISOR)
     @Get()
     getStatuses() {
         return this.statusService.getAll()
